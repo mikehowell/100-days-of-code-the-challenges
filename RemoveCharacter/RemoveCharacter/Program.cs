59051d2f85483c7ac06388c0eb91c663cfba26f6
Mike Howell <mikehowell@users.noreply.github.com>
2018-05-05 21:49:41 +1200

    added code  and unit tests to complete challenge.

---
+++ RemoveCharacter/RemoveCharacter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RemoveCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string");
+            var sentence = Console.ReadLine();
+            Console.WriteLine("Enter character to remove from string");
+            var character = Convert.ToChar(Console.ReadLine());
+
+            var remove = new Removal();
+            Console.WriteLine(remove.RemoveCharacterFromString(character, sentence));
+        }
+    }
+}
