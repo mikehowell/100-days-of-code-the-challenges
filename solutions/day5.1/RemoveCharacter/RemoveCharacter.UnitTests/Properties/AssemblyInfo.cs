6398115535a39e711e8b8313f13560476ef88730
Mike Howell <mikehowell@users.noreply.github.com>
2018-05-05 21:57:17 +1200

    enter code  and unit tests to complete challenge

---
+++ solutions/day5.1/RemoveCharacter/RemoveCharacter.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RemoveCharacter.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemoveCharacter.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec327193-8f96-46ff-8cff-3803fb2a846d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solutions/day5.1/RemoveCharacter/RemoveCharacter.UnitTests/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RemoveCharacter.UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void RemoveCharacterFromString_Success()
+        {
+            var sentence = "Hello World";
+            var character = 'l';
+
+            var remove = new Removal();
+            var expected = "Heo Word";
+
+            Assert.AreEqual(expected, remove.RemoveCharacterFromString(character, sentence));
+        }
+    }
+}
