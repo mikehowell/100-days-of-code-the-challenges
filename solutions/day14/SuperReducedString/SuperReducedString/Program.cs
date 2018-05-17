0b16b5d98fe93778f917f8ee0f9dd041b8739838
Mike Howell <mikehowell@users.noreply.github.com>
2018-05-17 20:59:39 +1200

    entered code to partially fulfill challenge

---
+++ solutions/day14/SuperReducedString/SuperReducedString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperReducedString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rd = new Reduce();
+
+            Console.WriteLine(rd.ReduceString("aabbcc")); //should return empty string
+            Console.WriteLine(rd.ReduceString("aaabccddd")); //does correctly return abd 
+            Console.WriteLine(rd.ReduceString("baab")); //should return empty string
+        }
+    }
+}
---
+++ solutions/day14/SuperReducedString/SuperReducedString/Reduce.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace SuperReducedString
+{
+    public class Reduce
+    {
+        public string ReduceString(string input)
+        {
+            string output = String.Empty;
+            StringBuilder sb = new StringBuilder(input);
+            int index = 0;
+
+            while (index < sb.Length - 1)
+            {
+                if (sb[index] == sb[index + 1])
+                {
+                    sb.Remove(index, 1);
+                    if (index > 0)
+                    {
+                        index--;
+                    }
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                output = sb.ToString();
+            }
+            else
+            {
+                output = "Empty String";
+            }
+
+            return output;
+        }
+    }
+}
