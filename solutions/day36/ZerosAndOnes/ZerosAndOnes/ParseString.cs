f9e0ef6d295e9a4502d893d610d3acb94bb448be
Mike Howell <mikehowell@users.noreply.github.com>
2018-06-12 21:53:26 +1200

    first (rather poor) draft attemp.

---
+++ solutions/day36/ZerosAndOnes/ZerosAndOnes/ParseString.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZerosAndOnes
+{
+    public class ParseString
+    {
+        public int ShortestString(string zerosAndOnes)
+        {
+            int count = 0;
+            var arrayOfZerosAndOnes = zerosAndOnes.ToCharArray();
+
+            char[] processedArray = ProcessArray(arrayOfZerosAndOnes);
+
+            foreach (var character in processedArray)
+            {
+                if (!character.Equals(' '))
+                {
+                    count += 1;
+                }
+            }
+
+            return count;
+        }
+
+        private Char[] ProcessArray(char[] arrayOfZerosAndOnes)
+        {
+            List<char> charArray = new List<char>();
+
+            for (int i = 0; i < arrayOfZerosAndOnes.Length; i++)
+            {
+                if (i == 0)
+                {
+                    charArray.Add(arrayOfZerosAndOnes[i]);
+                }
+                else
+                {
+                    if (arrayOfZerosAndOnes[i-1] != arrayOfZerosAndOnes[i])
+                    {
+                        charArray.Add(' ');
+                    }
+                    else
+                    {
+                        charArray.Add(arrayOfZerosAndOnes[i]);
+                    }
+                }
+            }
+
+            return charArray.ToArray<char>();
+        }
+    }
+}
---
+++ solutions/day36/ZerosAndOnes/ZerosAndOnes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace ZerosAndOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new ParseString();
+            int result = parser.ShortestString("01010");
+        }
+    }
+}
